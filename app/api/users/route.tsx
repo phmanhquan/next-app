cfe088e99d70d51b8f6afbead54cf4b69bd8d089
Quan Pham <Quan.Pham@vklink.vn>
2023-10-03 17:47:53 +0700

    getting data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Tí Đù" },
-    { id: 2, name: "Báo Thủ" },
-  ]);
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany();
+
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
