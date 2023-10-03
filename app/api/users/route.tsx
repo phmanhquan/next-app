f79701d71d50a2aae9357d41ef3a67bc677b2bf5
Quan Pham <Quan.Pham@vklink.vn>
2023-10-03 14:24:31 +0700

    create an object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -6,3 +6,10 @@ export function GET(request: NextRequest) {
     { id: 2, name: "Báo Thủ" },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "name is required" }, { status: 400 });
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
