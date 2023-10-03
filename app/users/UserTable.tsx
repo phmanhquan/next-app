9ef5f66ba73773e315dd9f3a88f21f48af9a7cba
Quan Pham <Quan.Pham@vklink.vn>
2023-10-03 09:43:30 +0700

    query string params

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import React from "react";
+import { sort } from "fast-sort";
 
 interface User {
   id: number;
@@ -7,23 +9,35 @@ interface User {
   email: string;
 }
 
-const UserTable = async () => {
+interface Props {
+  sortOrder: string;
+}
+
+const UserTable = async ({ sortOrder }: Props) => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
     cache: "no-store",
     next: { revalidate: 10 },
   });
   const users: User[] = await res.json();
 
+  const sortOrderUser = sort(users).asc(
+    sortOrder === "email" ? (user) => user.email : (user) => user.name
+  );
+
   return (
     <table className="table table-bordered">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Email</th>
+          <th>
+            <Link href="/users?sortOrder=name">Name</Link>
+          </th>
+          <th>
+            <Link href="/users?sortOrder=email">Email</Link>
+          </th>
         </tr>
       </thead>
       <tbody>
-        {users.map((user) => (
+        {sortOrderUser.map((user) => (
           <tr key={user.id}>
             <td> {user.name}</td>
             <td> {user.email}</td>
