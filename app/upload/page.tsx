19838bea5282818f520e61734bae8400e7a9904e
Quan Pham <Quan.Pham@vklink.vn>
2023-10-04 16:10:38 +0700

    show image

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,18 +1,42 @@
 "use client";
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
   return (
-    <CldUploadWidget uploadPreset="bwcymkhd">
-      {({ open }) => {
-        return (
-          <button className="btn btn-primary" onClick={() => open()}>
-            Upload an Image
-          </button>
-        );
-      }}
-    </CldUploadWidget>
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={270}
+          height={180}
+          alt="new image"
+        ></CldImage>
+      )}
+      <CldUploadWidget
+        uploadPreset="bwcymkhd"
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => {
+          return (
+            <button className="btn btn-primary" onClick={() => open()}>
+              Upload an Image
+            </button>
+          );
+        }}
+      </CldUploadWidget>
+    </>
   );
 };
 
