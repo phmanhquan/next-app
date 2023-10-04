74bbc0c2c16d02e6a73dc5a1cb14388f938ef943
Quan Pham <Quan.Pham@vklink.vn>
2023-10-04 15:36:31 +0700

    storing products

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../products/schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Milk", price: 2.5 },
-    { id: 2, name: "Bread", price: 3.5 },
-  ]);
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany();
+
+  return NextResponse.json(products);
 }
 
 export async function POST(request: NextRequest) {
@@ -16,11 +16,12 @@ export async function POST(request: NextRequest) {
   if (!validation.success)
     return NextResponse.json({ error: validation.error }, { status: 400 });
 
-  if (!body.name)
-    return NextResponse.json({ error: "name is required" }, { status: 400 });
+  const newProduct = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price,
+    },
+  });
 
-  return NextResponse.json(
-    { id: 1, name: body.name, price: body.price },
-    { status: 201 }
-  );
+  return NextResponse.json(newProduct, { status: 201 });
 }
