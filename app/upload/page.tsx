63cd1110252e0225de143264e673571377535670
Quan Pham <Quan.Pham@vklink.vn>
2023-10-04 16:23:49 +0700

    customize upload

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -21,6 +21,35 @@ const UploadPage = () => {
       )}
       <CldUploadWidget
         uploadPreset="bwcymkhd"
+        options={{
+          sources: ["local"],
+          multiple: false,
+          maxFiles: 5,
+          styles: {
+            palette: {
+              window: "#FFFFFF",
+              windowBorder: "#90A0B3",
+              tabIcon: "#0078FF",
+              menuIcons: "#5A616A",
+              textDark: "#000000",
+              textLight: "#FFFFFF",
+              link: "#0078FF",
+              action: "#FF620C",
+              inactiveTabIcon: "#0E2F5A",
+              error: "#F44235",
+              inProgress: "#0078FF",
+              complete: "#20B832",
+              sourceBg: "#E4EBF1",
+            },
+            fonts: {
+              default: null,
+              "'Fira Sans', sans-serif": {
+                url: "https://fonts.googleapis.com/css?family=Fira+Sans",
+                active: true,
+              },
+            },
+          },
+        }}
         onUpload={(result, widget) => {
           if (result.event !== "success") return;
 
