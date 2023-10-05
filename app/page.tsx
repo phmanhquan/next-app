57eccfa9f418cc16735ebea02d4c27156d182d09
Quan Pham <Quan.Pham@vklink.vn>
2023-10-05 17:10:48 +0700

    optimize

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import macos from "@/public/images/macos.jpg";
 import Image from "next/image";
+import { Metadata } from "next";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -30,4 +31,13 @@ export default async function Home() {
       ></Image>
     </main>
   );
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const product = await fetch("");
+
+  return {
+    title: "product.title",
+    description: "....",
+  };
 }
